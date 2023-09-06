d694cb20b351986c6f1e8291960e3a8bbbc9b401
AngelHell <angelhellwolf@gmail.com>
2023-09-06 17:39:47 +0800

    基础框架

---
+++ src/main/java/com/remiaft/angelhell/RMCTools.java
@@ -0,0 +1,17 @@
+package com.remiaft.angelhell;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RMCTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
