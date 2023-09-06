6288852440db5d9457bc48cf1d9556c02e405e54
AngelHell <angelhellwolf@gmail.com>
2023-09-06 17:49:06 +0800

    说明

--- src/main/java/com/remiaft/angelhell/RMCTools.java
+++ src/main/java/com/remiaft/angelhell/RMCTools.java
@@ -1,18 +1,27 @@
 package com.remiaft.angelhell;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RMCTools extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        //test
-        // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage(new String[] {
+                getPrefix() + "§a插件已加载",
+                "§a作者: §eAngelHell"
+                });
     }
+    public static String getPrefix(){
+        return "§a[RMCTools] §e";
+    }
+    //这只是一个实验性内容，这个东西的目的是让我练习 IDEA Minecraft plugin 和 GitHub 的使用
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage(new String[] {
+                getPrefix() + "§c插件已卸载",
+                "§a作者: §eAngelHell"
+        });
     }
 }
